7bae0ac5b576807336af5b94562b724e75224068
mavm0406 <mavm040601@gmail.com>
2024-04-30 12:46:47 +0200

    Intento de generar el resultado haciendo un metodo getInfo y pasadoselo al resultado

--- src/main/java/programa/Programa.java
+++ src/main/java/programa/Programa.java
@@ -8,7 +8,12 @@ import conexion_http.ConexionHTTP;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JRadioButton;
+import modelos.Joke;
+import modelos2.ListaChistes;
+import serviciojson.JsonService;
 
 /**
  *
@@ -19,6 +24,9 @@ public class Programa extends javax.swing.JFrame {
     private List<String> listaCategorias = new ArrayList<>();
     private String lenguaje = "";
     private String numChistes = "";
+    private Joke chiste;
+    private ListaChistes listaChistes;
+
     /**
      * Creates new form Programa
      */
@@ -48,9 +56,9 @@ public class Programa extends javax.swing.JFrame {
         jRadioButtonSpooky = new javax.swing.JRadioButton();
         jRadioButtonChrist = new javax.swing.JRadioButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextFieldNumChistes = new javax.swing.JTextField();
         jButtonGenerarChistes = new javax.swing.JButton();
         jRadioButtonTodasLasCat = new javax.swing.JRadioButton();
+        jComboNumChistes = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,7 +68,7 @@ public class Programa extends javax.swing.JFrame {
         jListIdioma.setBorder(null);
         jListIdioma.setForeground(new java.awt.Color(255, 255, 255));
         jListIdioma.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "en - English", "es - Español", "cs - Czech", "de - German", "fe - French", "pt - Portuguese" };
+            String[] strings = { "en - English", "es - Español", "cs - Czech", "de - German", "fr - French", "pt - Portuguese" };
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
@@ -76,7 +84,7 @@ public class Programa extends javax.swing.JFrame {
 
         jLabelTextoSelectCant.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
         jLabelTextoSelectCant.setForeground(new java.awt.Color(255, 255, 255));
-        jLabelTextoSelectCant.setText("3- Selecciona la cantidad de chistes a mostrar (de 1 a 10) :");
+        jLabelTextoSelectCant.setText("3- Selecciona la cantidad de chistes a mostrar:");
 
         jRadioButtonProg.setForeground(new java.awt.Color(255, 255, 255));
         jRadioButtonProg.setText("Programming");
@@ -152,13 +160,6 @@ public class Programa extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("GENERADOR DE CHISTES MALOS");
 
-        jTextFieldNumChistes.setEditable(false);
-        jTextFieldNumChistes.setBackground(new java.awt.Color(102, 102, 102));
-        jTextFieldNumChistes.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
-        jTextFieldNumChistes.setForeground(new java.awt.Color(255, 255, 255));
-        jTextFieldNumChistes.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextFieldNumChistes.setText("1");
-
         jButtonGenerarChistes.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
         jButtonGenerarChistes.setForeground(new java.awt.Color(153, 204, 255));
         jButtonGenerarChistes.setText("Generar chistes");
@@ -183,6 +184,11 @@ public class Programa extends javax.swing.JFrame {
             }
         });
 
+        jComboNumChistes.setBackground(new java.awt.Color(102, 102, 102));
+        jComboNumChistes.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
+        jComboNumChistes.setForeground(new java.awt.Color(255, 255, 255));
+        jComboNumChistes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -191,15 +197,25 @@ public class Programa extends javax.swing.JFrame {
                 .addGap(39, 39, 39)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabelTextoSelectCant, javax.swing.GroupLayout.PREFERRED_SIZE, 412, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextFieldNumChistes, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabelTextoSelectCant, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboNumChistes, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabelTextoSelecCat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(170, 170, 170))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 473, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabelTextoSelecIdioma, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(46, Short.MAX_VALUE))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(80, 80, 80)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(77, 77, 77)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -211,20 +227,9 @@ public class Programa extends javax.swing.JFrame {
                                     .addComponent(jRadioButtonPun)
                                     .addComponent(jRadioButtonChrist)
                                     .addComponent(jRadioButtonSpooky)))
-                            .addComponent(jRadioButtonTodasLasCat))
-                        .addContainerGap(223, Short.MAX_VALUE))
+                            .addComponent(jRadioButtonTodasLasCat)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 473, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabelTextoSelecIdioma, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(80, 80, 80)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(193, 193, 193)
+                        .addGap(188, 188, 188)
                         .addComponent(jButtonGenerarChistes)))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
@@ -237,9 +242,9 @@ public class Programa extends javax.swing.JFrame {
                 .addComponent(jLabelTextoSelecIdioma, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabelTextoSelecCat, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(2, 2, 2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelTextoSelecCat, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jRadioButtonTodasLasCat)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -253,13 +258,13 @@ public class Programa extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jRadioButtonDark)
                     .addComponent(jRadioButtonChrist))
-                .addGap(30, 30, 30)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabelTextoSelectCant, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextFieldNumChistes, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jComboNumChistes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jButtonGenerarChistes)
-                .addGap(46, 46, 46))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -286,13 +291,10 @@ public class Programa extends javax.swing.JFrame {
 
     private void jButtonGenerarChistesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonGenerarChistesMouseClicked
         // Lógica del programa
-        //Creo las URL base con ambos casos
-        String urlBase = "https://v2.jokeapi.dev/joke/Programming?lang=es";
-        String urlBase2 = "https://v2.jokeapi.dev/joke/Any?lang=es&amount=5";
 
         //Saco el lenguaje 
         lenguaje = jListIdioma.getSelectedValue();
-
+        System.out.println("lenguaje: " + lenguaje);
         //Hago una lista de JRadioButton y añado los de categoria para sacar los resultados
         List<JRadioButton> listaTmpJRadioButton = new ArrayList<>();
         listaTmpJRadioButton.add(jRadioButtonChrist);
@@ -312,14 +314,29 @@ public class Programa extends javax.swing.JFrame {
         }
 
         //Saco el numero de chistes
-        numChistes = jTextFieldNumChistes.getText().trim();
+        numChistes = jComboNumChistes.getSelectedItem().toString().trim();
+
+        System.out.println("numChistes: " + numChistes);
 
         try {
-            String fichero = ConexionHTTP.peticionHttpGet(urlBase);
-            String fichero2 = ConexionHTTP.peticionHttpGet(urlBase2);
+            String url = generarUrl1Chiste();
+            String fichero = ConexionHTTP.peticionHttpGet(url);
+
+            if (numChistes.equalsIgnoreCase("1")) {
+
+                chiste = (Joke) JsonService.stringToPojo(fichero, Joke.class);
+                System.out.println(chiste);
+                new ResultadoPrograma(this, true).setVisible(true);
+            } else {
+
+                listaChistes = (ListaChistes) JsonService.stringToPojo(fichero, ListaChistes.class);
+                new ResultadoPrograma(this, true).setVisible(true);
+            }
         } catch (IOException ex) {
-            //Logger.getLogger(Programa.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Programa.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+
     }//GEN-LAST:event_jButtonGenerarChistesMouseClicked
 
     private void jRadioButtonProgMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRadioButtonProgMouseClicked
@@ -404,6 +421,7 @@ public class Programa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonGenerarChistes;
+    private javax.swing.JComboBox<String> jComboNumChistes;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabelTextoSelecCat;
     private javax.swing.JLabel jLabelTextoSelecIdioma;
@@ -418,7 +436,6 @@ public class Programa extends javax.swing.JFrame {
     private javax.swing.JRadioButton jRadioButtonSpooky;
     private javax.swing.JRadioButton jRadioButtonTodasLasCat;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextFieldNumChistes;
     // End of variables declaration//GEN-END:variables
 
     public String generarUrl1Chiste() {
@@ -426,19 +443,62 @@ public class Programa extends javax.swing.JFrame {
             private List<String> listaCategorias = new ArrayList<>();
             private String lenguaje = "";
             private String numChistes = "";
-          */
+         */
         String url = "";
-        
-        if (listaCategorias.contains("Todas las opciones") || listaCategorias.isEmpty()) {
-            
+
+        //logica para la categoria de los chistes
+        if (listaCategorias.contains("Todas las opciones")
+                || listaCategorias.isEmpty()) {
+
             url = "https://v2.jokeapi.dev/joke/Any";
-        } else{
-            url = "https://v2.jokeapi.dev/joke/";
-            for (String categoria : listaCategorias) {
-                url += categoria + ",";
-            }
-            
+        } else if (listaCategorias.size() == 1
+                && !listaCategorias.contains("Todas las opciones")) {
+
+            url = "https://v2.jokeapi.dev/joke/" + listaCategorias.get(0);
+        } else {
+
+            url = "https://v2.jokeapi.dev/joke/" + String.join(",", listaCategorias);
         }
+
+        //logica para añadir el lenguaje
+        if (lenguaje.equalsIgnoreCase("en - English")) {
+
+            url += "?";
+        } else {
+            url += "?lang=";
+            String[] partesLenguaje = lenguaje.split(" - ");
+            url += partesLenguaje[0].trim();
+        }
+
+        url += "&amount=" + numChistes;
+
+        listaCategorias.removeAll(listaCategorias);
         return url;
+    }
+
+    public String getInfo() {
+
+        String resultado = "";
+
+        if (chiste != null) {
+            if (chiste.getType().equalsIgnoreCase("single")) {
+                resultado = chiste.getSetup();
+            } else {
+                resultado = chiste.getSetup()
+                        + "\n" + chiste.getDelivery();
+            }
+        } else if (listaChistes != null) {
+
+            for (modelos2.Joke joke : listaChistes.getJokes()) {
+                if (chiste.getType().equalsIgnoreCase("single")) {
+                    resultado += chiste.getSetup() + "\n" + "\n";
+                } else {
+                    resultado += chiste.getSetup()
+                            + "\n" + chiste.getDelivery() + "\n" + "\n";
+                }
+            }
+        }
+
+        return resultado;
     }
 }
